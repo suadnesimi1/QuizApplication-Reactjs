e9f08f7ded866d49a4547236d963f670d1e31dd9
suadnesimi1 <suadnesimi12@gmail.com>
2020-10-27 17:19:35 -0700

    Add all my files

---
+++ js/questions.js
@@ -0,0 +1,121 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What is Web Developer?",
+    answer: "A web developer is a programmer who specializes in the development of World Wide Web applications using a client–server model.",
+    options: [
+      "A programmer who is specializes in the web development applications using only client model.",
+      "Someone who is both creative and technically inclined, and uses both these attributes to build or redesign websites.",
+      "A web developer is a programmer who specializes in the development of World Wide Web applications using a client–server model.",
+      "A programmer who is specializes in the web development applications using only client model."
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is Reactjs?",
+    answer: "React is an open-source, front end, JavaScript library for building user interfaces or UI components.",
+    options: [
+      "It is a back-end programming language.",
+      "React is an open-source, front end, JavaScript library for building user interfaces or UI components.",
+      "High-level language.",
+      "React is an framework of Javascript for building mobile applications."
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is React Native?",
+    answer: "Is an open-source mobile application framework created by Facebook.It is used to develop applications for Android,iOS, Web,Windows and UWP.",
+    options: [
+      "Low-level programming language.",
+      "Is an Bootstrap library.",
+      "Is an open-source, front end, JavaScript library for building user interfaces or UI components.",
+      "Is an open-source mobile application framework created by Facebook.It is used to develop applications for Android,iOS, Web,Windows and UWP."
+    ]
+  },
+    {
+    numb: 4,
+    question: "Commonly used high-level programming languages?",
+    answer: "Python, Javascript, C#, C++, Visual Basic.",
+    options: [
+      "Python, Javascript, C#, C++, Visual Basic.",
+      "Bootstrap, HTML, CSS, C++, Visual Studio Code, Reactjs.",
+      "MySql, Java, Python, Javascript, C language.",
+      "C language, C#, C++, Bootstrap, Visual Basic."
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is Boostrap?",
+    answer: "Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development.",
+    options: [
+      "Bootstrap is a free and open-source Javascript framework directed at responsive, mobile-first front-end web development.",
+      "Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development.",
+      "Bootstrap is a free and open-source Python framework directed at responsive, mobile-first front-end web development.",
+      "Bootstrap is an Web Programming Language."
+    ]
+  },
+  {
+    numb: 6,
+    question: "What is PHP and what does PHP stand for?",
+    answer: "Hypertext PreProcessor or PHP is a general-purpose scripting language especially suited to web development.",
+    options: [
+      "eXecutable Multiplet Language or PHP is a general-purpose scripting language especially suited to web development. ",
+      "eXtensible Makeup Language or PHP is a general-purpose scripting language especially suited to web development. ",
+      "eXTra Multi-Programe Language or PHP is a general-purpose scripting language especially suited to web development.",
+      "Hypertext PreProcessor or PHP is a general-purpose scripting language especially suited to web development."
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "What is HTML?",
+    answer: "Hypertext Markup Language is the standard markup language for documents designed to be displayed in a web browser.",
+    options:[
+      "Hypertext Markup Language is the standard markup language for documents designed to be displayed in a web browser.",
+      "HTML is an programming language.",
+      "Hypertext Markup Language is a framework of Javascript.",
+      "Hypertext Markup Language is a style sheet language."
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "What is CSS?",
+    answer: "Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML.",
+    options:[
+      "Hypertext Markup Language is the standard markup language for documents designed to be displayed in a web browser.",
+      "Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML.",
+      "Cascading Style Sheets is the standard markup language for documents designed.",
+      "Hypetext Markup Language is a style sheet language used for describing the presentation of a document."
+    ]
+  },
+
+  {
+    numb: 9,
+    question:"What is Javascript?",
+    answer:"JavaScript, often abbreviated as JS, is a programming language that conforms to the ECMAScript specification.",
+    options:[
+      "Javascript is a style sheet language used for describing the presentation of a document written in a markup language such as HTML.",
+      "Javascript is a style sheet language used for describing the presentation of a document.",
+      "JavaScript, often abbreviated as JS, is a programming language that conforms to the ECMAScript specification.",
+      "Javascript is the standard markup language for documents designed to be displayed in a web browser."
+    ]
+  },
+
+  {
+    numb: 10,
+    question:"What is object-oriented programming (OOP)?",
+    answer:"Object-oriented programming is a programming paradigm based on the concept of objects, which can contain data and code: data in the form of fields, and code, in the form of procedures.",
+    options:[
+      "Is an open-source mobile application framework created by Facebook.It is used to develop applications for Android,iOS, Web,Windows and UWP.",
+      "Hypertext PreProcessor or PHP is a general-purpose scripting language especially suited to web development.",
+      "eXTra Multi-Programe Language or PHP is a general-purpose scripting language especially suited to web development.",
+      "Object-oriented programming is a programming paradigm based on the concept of objects, which can contain data and code: data in the form of fields, and code, in the form of procedures."
+
+    ]
+  },
+
+
+
+
+]
